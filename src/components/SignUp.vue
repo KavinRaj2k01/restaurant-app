2cdc318cf9e780df0731e8ce56403706e3a24b76
KavinRaj2k01 <kavinrajlab@protonmail.com>
2021-07-12 12:28:13 +0530

    added constraints to input field

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,16 +1,59 @@
 <template>
-<h1> Sign Up </h1>
-<form>
-  <input type="text" placeholder="Enter Your Name"><br>
-  <input type="email" placeholder="Enter Your Email"><br>
-  <input type="password" placeholder="Enter Your Password"><br>
-  <input type="password" placeholder="Enter Your Confirm Password"><br>
-  <input type="submit" id="subbut">
+<div><h1> Sign Up </h1>
+<form autocomplete="off" >
+  <input type="text" placeholder="Enter Your Name" required id="name"><br>
+  <input type="email" placeholder="Enter Your Email" required disabled id="email"><br>
+  <input type="password" placeholder="Enter Your Password" required disabled id="pass"><br>
+  <input type="password" placeholder="Enter Your Confirm Password" required disabled id="conpass"><br>
+  <input type="submit" id="subbut" disabled v-on:click="fun">
 </form>
+</div>
 </template>
 <script>
+
+onkeyup=()=>{
+    var name=document.getElementById('name').value;
+    if(name.length==" ")
+    {
+      document.getElementById("email").disabled = true;
+    }
+    else{
+        document.getElementById("email").disabled = false;
+        document.getElementById("email").style.borderColor = 'blue';
+        var email=document.getElementById('email').value;
+        if(email.length==" ")
+        {
+         document.getElementById("pass").disabled = true;
+         document.getElementById("conpass").disabled = true; 
+        }
+        else{
+          document.getElementById("pass").disabled = false; 
+          document.getElementById("conpass").disabled = false;
+          document.getElementById("pass").style.borderColor = 'blue';
+          document.getElementById("conpass").style.borderColor = 'blue'; 
+          var pass=document.getElementById('pass').value;
+          var conpass=document.getElementById('conpass').value;
+          if(pass==conpass)
+          {
+            document.getElementById("subbut").disabled = false;
+            document.getElementById("subbut").style.borderColor = 'blue';
+          }
+          else{
+            document.getElementById("subbut").disabled = true;
+            document.getElementById("subbut").style.borderColor = 'black';
+          }
+        }
+    }
+
+}
 export default{
-    name:'SignUp'
+    name:'SignUp',
+    methods:{
+      fun:function()
+    {
+    alert("hey not disabled");
+    }
+    }
 }
 </script>
 <style>
@@ -21,6 +64,11 @@ input{
     margin:10px 0px 10px 0px;
     font-weight:bold;
     font-size:15px;
+    border:2px solid black;
+    color:black;
+}
+#name
+{
     border:2px solid blue;
 }
 #subbut{
